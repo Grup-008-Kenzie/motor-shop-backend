984af5037a07f567f14c744b461c384ce77c5119
José Pedro <josepedro.bernardes1@gmail.com>
2023-08-09 18:15:58 -0300

    Feat: Fix entities and create the rest.

---
+++ src/entities/addresses.entity.ts
@@ -0,0 +1,29 @@
+import { Column, Entity, OneToOne, PrimaryGeneratedColumn } from "typeorm";
+import { User } from "./users.entity";
+
+@Entity("addresses")
+export class Address {
+    @PrimaryGeneratedColumn("uuid")
+    id: string
+
+    @Column({type:"varchar", length:8})
+    cep: string
+
+    @Column({type:"varchar", length:2})
+    state: string
+
+    @Column({type:"varchar", length:30})
+    city: string
+
+    @Column({type:"varchar", length:10})
+    street: string
+
+    @Column({type:"varchar", length:10})
+    number: string
+
+    @Column({type:"varchar", length:50})
+    complement: string
+
+    @OneToOne(() => User, user => user.address)
+    user: User;
+}
