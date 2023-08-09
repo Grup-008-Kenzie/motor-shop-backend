b80aa85cc5e1b68863826e105e97f2192a8db8e7
José Pedro <josepedro.bernardes1@gmail.com>
2023-08-09 12:37:04 -0300

    Refactor:

--- src/entities/cars.entity.ts
+++ src/entities/cars.entity.ts
@@ -12,12 +12,6 @@ import { Announcement } from "./announcements.entity";
     @Column({type:"varchar", length:20})
     model: string
 
-    @Column({type:"varchar", length:255})
-    front_image: string
-
-    @Column({type:"varchar", length:255})
-    first_image: string
-
     @OneToMany(() => Announcement, (announcements) => announcements.carId, { nullable: true })
     annoucements: Announcement[];
 }
