a0063e35e8b649114127589cd92562a876adfcb3
Carol-Rocha <caroldsr1@gmail.com>
2023-08-11 12:19:13 -0300

    feat: create data serialization of Address entity

---
+++ src/interfaces/address/index.ts
@@ -0,0 +1,5 @@
+import { z } from "zod"
+import { addressSchema, addressSchemaRequest } from "../../schemas/address"
+
+export type TAdress = z.infer<typeof addressSchema>
+export type TAdressRequest = z.infer<typeof addressSchemaRequest>
