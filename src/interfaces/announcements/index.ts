a0705cb314fcba24a69f54786fb5cddf1193d6ac
Carol-Rocha <caroldsr1@gmail.com>
2023-08-11 15:34:38 -0300

    feat: create announcement entity data serialization

--- src/interfaces/announcements/index.ts
+++ src/interfaces/announcements/index.ts
@@ -3,8 +3,10 @@ import { DeepPartial } from "typeorm"
 import {
   announcementSchema, 
   announcementSchemaRequest, 
+  announcementsSchema, 
   updatedAnnouncementSchema} from "../../schemas/announcements/index"
 
   export type TAnnouncement = z.infer<typeof announcementSchema>
+  export type TAnnouncementsResponse = z.infer<typeof announcementsSchema>
   export type TAnnouncementRequest = z.infer<typeof announcementSchemaRequest>
   export type TAnnouncementUpdateRequest = z.infer<typeof updatedAnnouncementSchema>
