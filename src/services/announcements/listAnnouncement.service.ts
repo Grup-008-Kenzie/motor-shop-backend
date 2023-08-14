17db0c6368cdceba42bebbab3c9814d4a90af66e
Carol-Rocha <caroldsr1@gmail.com>
2023-08-14 10:07:01 -0300

    feat: add pagination to announcement listing

--- src/services/announcements/listAnnouncement.service.ts
+++ src/services/announcements/listAnnouncement.service.ts
@@ -2,12 +2,15 @@ import { Repository } from "typeorm";
 import { Announcement } from "../../entities"
 import { AppDataSource } from "../../data-source";
 
-export const listAnnouncementsService = async (): Promise<Announcement[] > => {
+export const listAnnouncementsService = async (): Promise<Announcement[]> => {
   const announcementRepository: Repository<Announcement> = AppDataSource.getRepository(Announcement)
 
   const announcements: Announcement[] = await announcementRepository
-    .createQueryBuilder('anns')
-    .getMany()
+  .createQueryBuilder('anns')
+  .orderBy('anns.car', 'ASC')
+  .skip(0)
+  .take(16)
+  .getMany()
 
   return announcements
 }
