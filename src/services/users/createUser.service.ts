5637168801a70a27b00a1b85c21e146670b420c2
José Pedro <josepedro.bernardes1@gmail.com>
2023-08-22 14:26:57 -0300

    refactor: separating logic into files for easier maintenance.

---
+++ src/services/users/createUser.service.ts
@@ -0,0 +1,32 @@
+import { AppError } from "../../errors/AppError";
+import { addressRepository, userRepository } from "../../repositories";
+import { userSchema } from "../../schemas/users";
+import { Address, User } from "../../entities";
+
+export const createUserService = async (data: any) => {
+    const { email, cpf, phone_number, address, birthdate, description, name, password } = data
+
+    const findEmail = await userRepository.findOne({ where: { email } });
+    if (findEmail) throw new AppError("Email is already in use", 409);
+
+    const findCPF = await userRepository.findOne({ where: { cpf } });
+    if (findCPF) throw new AppError("CPF is already in use", 409);
+
+    const newAddress:Address = addressRepository.create({
+        cep: address.cep,
+        state: address.state,
+        city: address.city,
+        number: address.number,
+        street: address.street,
+        complement: address.complement
+    });
+
+    await addressRepository.save(newAddress);
+
+    const newUser: User = userRepository.create({
+        email, cpf, phone_number, address: newAddress, birthdate, name, password, description
+    })
+
+    await userRepository.save(newUser)
+    return userSchema.parse(newUser)
+}
