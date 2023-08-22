ea00cf3f89e41a06a19de92f190f91bf09aab6e7
José Pedro <josepedro.bernardes1@gmail.com>
2023-08-22 19:21:17 -0300

    fix: some bugs

--- src/services/users/deleteUser.service.ts
+++ src/services/users/deleteUser.service.ts
@@ -3,5 +3,5 @@ import { userRepository } from "../../repositories";
 export const deleteUserService = async (userId: string) => {
   const user = await userRepository.findOneByOrFail({ id: userId });
   await userRepository.remove(user);
-  return "User deleted successfully.";
+  return;
 };
