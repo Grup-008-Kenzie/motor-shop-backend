49e18e7b0d283f2db3348cd5e11a6521187daf6f
Carol-Rocha <caroldsr1@gmail.com>
2023-08-31 09:58:42 -0300

    fix: fix User entity data typings and serialization

--- src/interfaces/users/index.ts
+++ src/interfaces/users/index.ts
@@ -1,14 +1,25 @@
 import {
   loginSchema,
   userCreateSchema,
+  userResponseSchema,
   userSchema,
   userUpdateSchema,
+  usersResponseSchema,
 } from "../../schemas/users";
 import { z } from "zod";
 
 type TUser = z.infer<typeof userSchema>;
 type TUserCreate = z.infer<typeof userCreateSchema>;
 type TUserUpdate = z.infer<typeof userUpdateSchema>;
+type TUserResponse = z.infer<typeof userResponseSchema>;
+type TUsersResponse = z.infer<typeof usersResponseSchema>;
 type TLogin = z.infer<typeof loginSchema>;
 
-export { TUser, TUserCreate, TUserUpdate, TLogin };
+export {
+  TUser,
+  TUserCreate,
+  TUserUpdate,
+  TLogin,
+  TUserResponse,
+  TUsersResponse,
+};
