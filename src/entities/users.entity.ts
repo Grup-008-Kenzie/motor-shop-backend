6547b78aa7c8dee5207f88fd91bec1219caceca2
Carol-Rocha <caroldsr1@gmail.com>
2023-08-31 11:14:10 -0300

    fix: fix user entity birthday property

--- src/entities/users.entity.ts
+++ src/entities/users.entity.ts
@@ -7,68 +7,68 @@ import {
   BeforeInsert,
   BeforeUpdate,
   OneToOne,
-  JoinColumn
-} from 'typeorm'
-import { getRounds, hashSync } from 'bcryptjs'
-import { Address } from './addresses.entity'
-import { Announcement } from './announcements.entity'
-import { Comment } from './comments.entity'
+  JoinColumn,
+} from "typeorm";
+import { getRounds, hashSync } from "bcryptjs";
+import { Address } from "./addresses.entity";
+import { Announcement } from "./announcements.entity";
+import { Comment } from "./comments.entity";
 
-@Entity('users')
+@Entity("users")
 export class User {
-  @PrimaryGeneratedColumn('uuid')
-  id: string
+  @PrimaryGeneratedColumn("uuid")
+  id: string;
 
-  @Column({ type: 'varchar', length: 90 })
-  name: string
+  @Column({ type: "varchar", length: 90 })
+  name: string;
 
-  @Column({ type: 'varchar', length: 125, unique: true })
-  email: string
+  @Column({ type: "varchar", length: 125, unique: true })
+  email: string;
 
-  @Column({ type: 'varchar', length: 11, unique: true })
-  cpf: string
+  @Column({ type: "varchar", length: 11, unique: true })
+  cpf: string;
 
-  @Column({ type: 'varchar', length: 15, unique: true })
-  phone_number: string
+  @Column({ type: "varchar", length: 15, unique: true })
+  phone_number: string;
 
-  @CreateDateColumn({ type: 'varchar', length: 10 })
-  birthdate: string
+  @CreateDateColumn({ type: "varchar", length: 10 })
+  birthdate: string;
 
-  @Column({ type: 'text', nullable: true })
-  description: string | null
+  @Column({ type: "text", nullable: true })
+  description: string | null;
 
-  @Column({ type: 'boolean', default: false })
-  is_seller: boolean
+  @Column({ type: "boolean", default: false })
+  is_seller: boolean;
 
-  @Column({ type: 'boolean', default: false })
-  admin: boolean
+  @Column({ type: "boolean", default: false })
+  admin: boolean;
 
-  @Column({ type: 'varchar', length: 120 })
-  password: string
+  @Column({ type: "varchar", length: 120 })
+  password: string;
 
   @OneToOne(() => Address, (address) => address.user, { cascade: true })
   @JoinColumn()
-  address: Address
+  address: Address;
 
   @OneToMany(() => Announcement, (ann) => ann.seller, {
     nullable: true,
-    cascade: true
+    cascade: true,
   })
-  announcement: Announcement[]
+  announcement: Announcement[];
 
   @OneToMany(() => Comment, (cmt) => cmt.user, {
     nullable: true,
-    cascade: true
+    cascade: true,
   })
-  comment: Comment
+  comment: Comment;
 
   @BeforeInsert()
   @BeforeUpdate()
   hashPassword() {
-    const isEncrypted = getRounds(this.password)
+    const isEncrypted = getRounds(this.password);
 
     if (!isEncrypted) {
-      this.password = hashSync(this.password, 10)
+      this.password = hashSync(this.password, 10);
     }
   }
 }
