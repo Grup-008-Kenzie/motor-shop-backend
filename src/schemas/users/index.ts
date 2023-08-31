23a4098b1e8bc848b24cb2b6f685a6d758274d76
Carol-Rocha <caroldsr1@gmail.com>
2023-08-31 09:58:42 -0300

    fix: fix User entity data typings and serialization

--- src/schemas/users/index.ts
+++ src/schemas/users/index.ts
@@ -1,34 +1,34 @@
-import { z } from 'zod';
-import { addressSchemaRequest } from '../address';
-import { announcementSchema } from '../announcements';
+import { z } from "zod";
+import { addressSchemaRequest } from "../address";
+import { announcementSchema } from "../announcements";
 
 export const userSchema = z.object({
-    id: z.string(),
-    name: z.string().max(90),
-    email: z.string().email().max(125),
-    cpf: z.string().max(11),
-    phone_number: z.string().max(15),
-    birthdate: z.string().length(8),
-    description: z.string().nullable(),
-    is_seller: z.boolean(),
-    admin: z.boolean(),
-    password: z.string().max(120),
-    address: addressSchemaRequest,
-    announcement: announcementSchema.optional()
+  id: z.string(),
+  name: z.string().max(90),
+  email: z.string().email().max(125),
+  cpf: z.string().max(11),
+  phone_number: z.string().max(15),
+  birthdate: z.string().length(8),
+  description: z.string().nullable(),
+  is_seller: z.boolean(),
+  admin: z.boolean(),
+  password: z.string().max(120),
+  address: addressSchemaRequest,
+  announcement: announcementSchema.optional(),
 });
 
 export const userCreateSchema = userSchema.omit({
   id: true,
   is_seller: true,
   announcement: true,
-  admin: true
+  admin: true,
 });
 
 export const userResponseSchema = userSchema.omit({
   password: true,
 });
 
-export const usersResponseSchema = userResponseSchema.array();
+export const usersResponseSchema = z.array(userResponseSchema);
 export const userUpdateSchema = userCreateSchema.partial().omit({
   cpf: true,
 });
